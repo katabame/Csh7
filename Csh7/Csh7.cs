84c51e0411cb390dae0ef001aa53bebd96e9e216
Katabame Ayame <katabamia@gmail.com>
2017-04-06 05:36:33 +0900

    Add: Basic Features
    Add: GetInfo()
    Add: Push()

---
+++ Csh7/Csh7.cs
@@ -0,0 +1,80 @@
+﻿using Codeplex.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh7
+{
+    public class Push7
+    {
+        const string END_POINT = "https://api.push7.jp/api/v1/"; // must ending with slash
+
+        /// <summary>
+        /// get push7 Application infomation with app number
+        /// </summary>
+        /// <param name="appNumber"></param>
+        /// <param name="datakey"></param>
+        /// <returns>specified datakey value or exception</returns>
+        public static string GetInfo(string appNumber, string datakey)
+        {
+            WebClient client = new WebClient();
+            var response = client.DownloadString(END_POINT + appNumber + "/head");
+            var json = DynamicJson.Parse(response);
+
+            if(json.IsDefined("error"))
+            {
+                throw new ArgumentException("An error occured. Reason: " + json["error"], "appNumber");
+            }
+
+            if(!json.IsDefined(datakey))
+            {
+                throw new ArgumentException("Specified datakey is not found. datakey: " + datakey, "datakey");
+            }
+
+            return json.datakey;
+        }
+
+        // delay push is not write in official docment. do not implementing.
+        /// <summary>
+        /// create push via push7 with some argments
+        /// </summary>
+        /// <param name="appNumber"></param>
+        /// <param name="title"></param>
+        /// <param name="content"></param>
+        /// <param name="iconURL"></param>
+        /// <param name="URL"></param>
+        /// <param name="apikey"></param>
+        /// <returns>pushid or exception</returns>
+        public static string Push(string appNumber, string title, string content, string iconURL, string URL, string apikey)
+        {
+            var obj = new
+            {
+                title = title,
+                body = content,
+                icon = iconURL,
+                url = URL,
+                apikey = apikey
+            };
+
+            var requestBody = DynamicJson.Serialize(obj);
+            Console.WriteLine(requestBody);
+
+            WebClient client = new WebClient();
+            client.Headers[HttpRequestHeader.ContentType] = "application/json;charset=UTF-8";
+            client.Encoding = Encoding.UTF8;
+
+            var response = client.UploadString(END_POINT + appNumber + "/send", requestBody);
+            var json = DynamicJson.Parse(response);
+
+            if (json.IsDefined("error"))
+            {
+                throw new ArgumentException("An error occured. Reason: " + json["error"], "appNumber");
+            }
+
+            return json.pushid;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Csh7
 Cs + Push7 = Csh7
+
+Csh7 is very simple Push7 library for C# users.
+
+**This project is not official!**
+
+### Usage
+```Cs
+using Csh7;
+
+push7.GetInfo(string apikey, string datakey); // Get Application Info
+
+push7.Push(string appNumber, string title, string content, string iconURL, string URL, string apikey); // Create Push
+```
+
+_little more real example_
+```Cs
+using Csh7;
+
+push7.GetInfo("1145141919810", "name");
+// will return specified key value or any exception.
+
+push7.Push("1145141919810", "test", "This is test.", "http://example.com/icon.png", "http://example.com", "1145141919810INMUC");
+// will return pushid or any exception.
+```
+
+for get more infomation about push7 API, please go [here](https://esa-pages.io/p/sharing/3426/posts/203/80ff5595487df69dfe38.html)!
+
+### Nuget?
+Currentry No.  
+but WIP.
+
+### Special Thanks
+* [Push7](https://push7.jp/): Awesome Service!
+* [DynamicJson](https://dynamicjson.codeplex.com/): Using in project.
+* [a-r-g-v/push7-python](https://github.com/a-r-g-v/push7-python): Coolest official python library, this project inspired from this!
